e8df1b22493f4255b0a04fdcccc25d80f0cf2568
Style-spot <arshakm068@gmail.com>
2026-08-23 13:06:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2397,15 +2397,11 @@ async function handleCreatePayment(
 
           gatewayOrderId:
             existing.gateway_order_id
-          keyId:
-              process.env.RAZORPAY_KEY_ID
-    console.log("RAZORPAY KEY CHECK:", 
-     process.env.RAZORPAY_KEY_ID ? "PRESENT" : "MISSING");
         }
-      }
+      } 
     );
   }
-
+  
   /*
   =======================================================
   CREATE RAZORPAY ORDER
